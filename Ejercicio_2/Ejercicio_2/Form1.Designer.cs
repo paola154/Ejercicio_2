ae7f8bad22f7258490889e3caf365acb736040d2
paola154 <paolaperdomo110@gmail.com>
2022-02-16 20:15:28 -0600

    Ejercicio finalizado
    Terminado

---
+++ Ejercicio_2/Ejercicio_2/Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace Ejercicio_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.NombreEstudiantetextBox = new System.Windows.Forms.TextBox();
+            this.EdadtextBox = new System.Windows.Forms.TextBox();
+            this.Mostrarbutton = new System.Windows.Forms.Button();
+            this.Guardarbutton = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(52, 29);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(100, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Nombre Estudiante:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(117, 59);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Edad:";
+            // 
+            // NombreEstudiantetextBox
+            // 
+            this.NombreEstudiantetextBox.Location = new System.Drawing.Point(158, 29);
+            this.NombreEstudiantetextBox.Name = "NombreEstudiantetextBox";
+            this.NombreEstudiantetextBox.Size = new System.Drawing.Size(100, 20);
+            this.NombreEstudiantetextBox.TabIndex = 7;
+            // 
+            // EdadtextBox
+            // 
+            this.EdadtextBox.Location = new System.Drawing.Point(158, 59);
+            this.EdadtextBox.Name = "EdadtextBox";
+            this.EdadtextBox.Size = new System.Drawing.Size(100, 20);
+            this.EdadtextBox.TabIndex = 8;
+            // 
+            // Mostrarbutton
+            // 
+            this.Mostrarbutton.Location = new System.Drawing.Point(277, 110);
+            this.Mostrarbutton.Name = "Mostrarbutton";
+            this.Mostrarbutton.Size = new System.Drawing.Size(75, 23);
+            this.Mostrarbutton.TabIndex = 9;
+            this.Mostrarbutton.Text = "Mostrar";
+            this.Mostrarbutton.UseVisualStyleBackColor = true;
+            this.Mostrarbutton.Click += new System.EventHandler(this.Mostrarbutton_Click);
+            // 
+            // Guardarbutton
+            // 
+            this.Guardarbutton.Location = new System.Drawing.Point(277, 27);
+            this.Guardarbutton.Name = "Guardarbutton";
+            this.Guardarbutton.Size = new System.Drawing.Size(75, 23);
+            this.Guardarbutton.TabIndex = 11;
+            this.Guardarbutton.Text = "Guardar";
+            this.Guardarbutton.UseVisualStyleBackColor = true;
+            this.Guardarbutton.Click += new System.EventHandler(this.Guardarbutton_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 107);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(259, 82);
+            this.listBox1.TabIndex = 12;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(431, 255);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.Guardarbutton);
+            this.Controls.Add(this.Mostrarbutton);
+            this.Controls.Add(this.EdadtextBox);
+            this.Controls.Add(this.NombreEstudiantetextBox);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Name = "Form1";
+            this.Text = "Ejercicio_2";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox NombreEstudiantetextBox;
+        private System.Windows.Forms.TextBox EdadtextBox;
+        private System.Windows.Forms.Button Mostrarbutton;
+        private System.Windows.Forms.Button Guardarbutton;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ Ejercicio_2/Ejercicio_2/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Collections;
+
+namespace Ejercicio_2
+{
+    public partial class Form1 : Form
+    {
+        // Hice uso de ArrayList para que el usuario tenga acceso a agregar cuantos estudiantes desee.
+        // y evitando en lugar de pedir el tamaño para cada arreglo.
+        ArrayList Estudiantes;
+        ArrayList Edades;
+
+        public Form1()
+        {
+            InitializeComponent();
+            // Inicializo los dos arreglos a usar 
+            Estudiantes = new ArrayList();
+            Edades = new ArrayList();
+        }
+
+        private void Guardarbutton_Click(object sender, EventArgs e)
+        {
+            // Aquí el usuario podra ingresar los nombres de los estudiantes y su edad. 
+            Estudiantes.Add (NombreEstudiantetextBox.Text.ToString());
+            Edades.Add(EdadtextBox.Text);
+
+            // Se usa la instruccion .clear para limpiar el campo y poder agregar otro nuevo 
+            NombreEstudiantetextBox.Clear();
+            EdadtextBox.Clear();    
+        }
+
+        private void Mostrarbutton_Click(object sender, EventArgs e)
+        {
+            // Con estas instrucciones podremos mostrar los datos antes ingresados en un listBox
+            // Haciendo uso de un for.
+            for(int i =0; i < Estudiantes.Count;i++)
+            {
+                listBox1.Items.Add("El alumno: " + Estudiantes[i] + "Tiene " + Edades[i] + " Años de edad");
+            }
+        }
+    }
+}
